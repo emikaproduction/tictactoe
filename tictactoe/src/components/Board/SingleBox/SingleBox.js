b8e0f56500e1d09e303c3850b5b5155c41a9a4d7
Paweł Mikicin <mikuspawel2@gmail.com>
2019-06-01 11:38:24 +0200

    Create single box component with init styles

--- tictactoe/src/components/Board/SingleBox/SingleBox.js
+++ tictactoe/src/components/Board/SingleBox/SingleBox.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function SingleBox() {
+const SingleBox = ({ value }) => {
+  const handleOnClick = () => {
+    console.log(value);
+  }
   return (
-    <div className="singleBox">
-      SingleBox
-    </div>
+    <Box className="singleBox" onClick={handleOnClick}></Box>
   );
 }
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+  font-size: 3em;
+`;
 
 export default SingleBox;
