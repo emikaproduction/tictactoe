75da4a355d9f662a88af814b3be028e7a3cab561
Paweł Mikicin <mikuspawel2@gmail.com>
2019-06-02 18:30:14 +0200

    Create board component

--- tictactoe/src/components/Status/Status.js
+++ tictactoe/src/components/Status/Status.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { generalBlue, generalRed } from './../../lib/styles/variables';
 
-function Status() {
+const Status = ({ currentPlayer, playerName, isWinner }) => {
   return (
-    <div className="status">
-      Status
-    </div>
+    <StyledStatus className={currentPlayer + (isWinner ? ' winner' : ' ')}>
+      Gracz {playerName} {isWinner && 'Wygrał!'}
+    </StyledStatus>
   );
 }
+
+const StyledStatus = styled.div`
+  padding: 10px 15px;
+  font-size: 2em;
+  color: white;
+  margin-bottom: 15px;
+  &.firstPlayer {
+    background-color: ${generalRed};
+  }
+  &.secondPlayer {
+    background-color: ${generalBlue};
+  }
+  &.winner {
+    background-color: green;
+  }
+`;
 
 export default Status;
