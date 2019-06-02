2f85456d03116518975d5afcc13f2007dd1e5a94
Paweł Mikicin <mikuspawel2@gmail.com>
2019-06-02 17:30:22 +0200

    Refactor single box component

--- tictactoe/src/components/Board/SingleBox/SingleBox.js
+++ tictactoe/src/components/Board/SingleBox/SingleBox.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import xIcon from '../../../lib/images/x-icon.png';
+import oIcon from '../../../lib/images/o-icon.png';
 
-const SingleBox = ({ value }) => {
-  const handleOnClick = () => {
-    console.log(value);
-  }
+const SingleBox = props => {
+  const { handleOnClick, value } = props;
+
   return (
-    <Box className="singleBox" onClick={handleOnClick}></Box>
+    <StyledButton value={value} onClick={handleOnClick} />
   );
-}
+};
 
-const Box = styled.div`
+const StyledButton = styled.button`
   display: flex;
   justify-content: center;
+  background-color: white;
   align-items: center;
-  border: 2px solid black;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   font-size: 3em;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  &:focus {
+    outline: none;
+  }
+  &.firstPlayer {
+    background-image: url('${xIcon}');
+    background-size: 100%;
+    &:hover {
+      background-color: white;
+      cursor: default;
+    }
+  }
+  &.secondPlayer {
+    background-image: url('${oIcon}');
+    background-size: 100%;
+    &:hover {
+      background-color: white;
+      cursor: default;
+    }
+  }
 `;
 
 export default SingleBox;
