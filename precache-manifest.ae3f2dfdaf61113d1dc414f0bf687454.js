d7f91bed2bbb3c4131841b1eb9dec2077d9e7aa1
Paweł Mikicin <mikuspawel2@gmail.com>
2019-06-02 18:07:22 +0200

    Updates

---
+++ precache-manifest.ae3f2dfdaf61113d1dc414f0bf687454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82028d0bd6aea6d5d48cc3a7b20a9563",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "1fae7b9d22ceafde6ce1",
+    "url": "/tictactoe/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "46bb416a3326ef3b0daf",
+    "url": "/tictactoe/static/js/2.bd9d51e3.chunk.js"
+  },
+  {
+    "revision": "1fae7b9d22ceafde6ce1",
+    "url": "/tictactoe/static/js/main.db663a27.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  },
+  {
+    "revision": "fbcd6f0d6b4d25f4781a9c8432f1c82e",
+    "url": "/tictactoe/static/media/logo.fbcd6f0d.png"
+  }
+]);
